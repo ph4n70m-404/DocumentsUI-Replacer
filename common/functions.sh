711e129c90f027a663be6500df91a3b1873a2a30
ph4n70m-404 <87523412+ph4n70m-404@users.noreply.github.com>
2021-07-16 06:28:07 -0500

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -106,10 +106,10 @@ prop_process() {
 }
 
 # Credits
-#ui_print "**************************************"
-#ui_print "*   MMT Extended by Zackptg5 @ XDA   *"
-#ui_print "**************************************"
-#ui_print " "
+ui_print "**************************************"
+ui_print "*   Based off of MMT Extended by Zackptg5 @ XDA   *"
+ui_print "**************************************"
+ui_print " "
 
 # Check for min/max api version
 [ -z $MINAPI ] || { [ $API -lt $MINAPI ] && abort "! Your system API of $API is less than the minimum api of $MINAPI! Aborting!"; }
