d5f4abf16ba7658cbca6f37797897d1c8f66f035
ph4n70m-404 <87523412+ph4n70m-404@users.noreply.github.com>
2021-07-22 16:08:00 -0500

    Add files via upload

---
+++ common/install.sh
@@ -0,0 +1,14 @@
+#Volume key selection
+ echo "- Select Version -"
+ echo "Choose which DocumentsUI version you want installed:"
+ echo "grey dark theme is from LineageOS, black dark theme is from dotOS"
+ echo "Vol+ = grey dark theme, Vol- = black dark theme"
+if chooseport; then
+  echo "grey dark theme chosen"
+rm -rf $MODPATH/temp
+else
+  echo "black dark theme chosen"
+cp -f $MODPATH/temp/DocumentsUI.apk $MODPATH/system/priv-app/DocumentsUI/DocumentsUI.apk
+rm -rf $MODPATH/temp
+fi
+
